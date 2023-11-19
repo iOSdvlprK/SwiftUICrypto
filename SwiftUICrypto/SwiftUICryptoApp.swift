2a62883245c4666105647f12fb05f165c040af39
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-11-19 21:26:39 +0900

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by joe on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
