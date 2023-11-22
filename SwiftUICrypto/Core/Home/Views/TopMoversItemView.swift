b0c27974dc3cf884ebb7bb6a60bfee648a41bae9
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-11-22 23:45:02 +0900

    coin images with Kingfisher; number formatting

--- SwiftUICrypto/Core/Home/Views/TopMoversItemView.swift
+++ SwiftUICrypto/Core/Home/Views/TopMoversItemView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct TopMoversItemView: View {
     let coin: Coin
@@ -13,7 +14,7 @@ struct TopMoversItemView: View {
     var body: some View {
         VStack(alignment: .leading) {
             // image
-            Image(systemName: "bitcoinsign.circle.fill")
+            KFImage(URL(string: coin.image))
                 .resizable()
                 .frame(width: 32, height: 32)
                 .foregroundColor(.orange)
@@ -25,15 +26,15 @@ struct TopMoversItemView: View {
                     .font(.caption)
                     .fontWeight(.bold)
                 
-                Text("\(coin.currentPrice)")
+                Text(coin.currentPrice.toCurrency())
                     .font(.caption)
                     .foregroundColor(.gray)
             }
             
             // coin percent change
-            Text("\(coin.priceChangePercentage24H)")
+            Text(coin.priceChangePercentage24H.toPercentString())
                 .font(.title2)
-                .foregroundColor(.green)
+                .foregroundColor(coin.priceChangePercentage24H > 0 ? .green : .red)
         }
         .frame(width: 140, height: 140)
         .overlay(
